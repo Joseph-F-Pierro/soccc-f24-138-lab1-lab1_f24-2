905bc20867f4deef6c43c0b1f837657761a423b9
Joseph F Pierro <127888366+Joseph-F-Pierro@users.noreply.github.com>
2024-09-07 19:55:18 -0400

    Update temp_converter.py
    third attempt at this Lab 1

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -2,11 +2,12 @@
 
 # process
 def c2f(c):
-    return c * 9 / 5 + 32
+            # below is my correction using PEMDAS
+    return (c * 9 // 5) + 32
 
 def main(cel):
     return c2f(cel)
-
+''' Joseph, course 138, Date 09072024, class section EG.'''
 if __name__ == "__main__":
     cel = 100         # input
     print(main(cel))  # output
